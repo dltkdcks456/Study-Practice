ac25b9a420d054421337dc66faa4b0ef6873e953
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-01 12:04:05 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 924 ms, Memory: 125424 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,4 +1,6 @@
 import sys
 N = int(sys.stdin.readline())
-num_list = sorted([int(sys.stdin.readline()) for i in range(N)])
-print('\n'.join(map(str, num_list)))
+num_list = [False] * 2000001
+for i in range(N):
+    num_list[int(sys.stdin.readline())] = True
+print('\n'.join([str(num) for num in range(-1000000 , 1000001) if num_list[num] == True]))
