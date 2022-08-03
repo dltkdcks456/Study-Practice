cc3bb48759ed9740a842cc6217619c27c0530895
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-03 12:11:32 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,20 +1,16 @@
 import sys
 
-n, m = list(map(int, sys.stdin.readline().split()))
+n, m = map(int, sys.stdin.readline().split())
 
 A = set()
 B = set()
 
-for i in range(1, n + 1):
+for i in range(1, max(n, m) + 1):
     if n % i == 0:
         A.add(i)
-
-for j in range(1, m + 1):
-    if m % j == 0:
-        B.add(j)
+    if m % i == 0:
+        B.add(i)
 
 ans1 = sorted(list(A & B))[-1]
-ans2 = ans1 * (n // ans1) * (m // ans1)
-
 print(ans1)
-print(ans2)
+print(ans1 * (n // ans1) * (m // ans1))
