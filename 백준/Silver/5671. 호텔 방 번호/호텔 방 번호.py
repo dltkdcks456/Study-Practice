6c43968a21a150934f4e6e39b8fdef3947c35559
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-03 09:58:58 +0900

    [Silver V] Title: 호텔 방 번호, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
+++ 백준/Silver/5671. 호텔 방 번호/호텔 방 번호.py
@@ -1,21 +1,12 @@
 import sys
-while True:
-    try:
-        N, M = map(int, sys.stdin.readline().split())
-        cnt = 0
 
-        def sol(x):
-            num_list = [0] * 10
-            global cnt
-            for i in list(map(int,str(x))):
-                if num_list[i] == True:
-                    cnt = cnt + 1
-                    break
-                else:
-                    num_list[i] = True
+c = 0
+a = [0] * 5001
+for i in range(5001):
+    if len(str(i)) == len(set(str(i))):
+        c += 1
+    a[i] = c
 
-        for j in range(N, M + 1):
-            sol(j)
-        print((M - N + 1) - cnt)
-    except:
-        break
+for e in sys.stdin.readlines():
+    n, m = map(int, e.split())
+    print(a[m] - a[n - 1])
