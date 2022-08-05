db899dc5248cecd8c592ab38312201db46655943
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-06 01:39:08 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 364 ms, Memory: 44692 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,4 +1,5 @@
-num_list = sorted(list(tuple(map(int, input().split())) for n in range(int(input()))))
+import sys
+num_list = sorted(list(tuple(map(int, sys.stdin.readline().split())) for n in range(int(sys.stdin.readline()))))
 
 for x in num_list:
     print(' '.join(map(str, x)))
