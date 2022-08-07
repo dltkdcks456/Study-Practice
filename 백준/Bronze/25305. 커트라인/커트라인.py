d2fff8f11e9dd68d6db0cee858999000bedee2da
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-07 17:21:49 +0900

    [Bronze II] Title: 커트라인, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.py
+++ 백준/Bronze/25305. 커트라인/커트라인.py
@@ -1,12 +1,4 @@
 import sys
 N, k = map(int, sys.stdin.readline().split())
-score = list(map(int, sys.stdin.readline().split()))
-
-for i in range(N - 1, N - k - 1, -1):
-    max = i
-    for j in range(i):
-        if score[max] < score[j]:
-            max = j
-    score[max], score[i] = score[i], score[max]
-
+score = sorted(list(map(int, sys.stdin.readline().split())))
 print(score[N - k])
