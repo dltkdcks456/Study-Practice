af72fd51267c4e2d13875de37769737234aec6bf
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-07 17:44:05 +0900

    [Silver III] Title: 문자열 집합, Time: 904 ms, Memory: 36004 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,8 +1,7 @@
 N, M = map(int,input().split())
-N_str = set(input() for i in range(N))
-M_str = [input() for j in range(M)]
+text_set = set(input() for i in range(N))
 cnt = 0
-for text in N_str:
-    cnt += M_str.count(text)
-
+for i in range(M):
+    if input() in text_set:
+        cnt += 1
 print(cnt)
