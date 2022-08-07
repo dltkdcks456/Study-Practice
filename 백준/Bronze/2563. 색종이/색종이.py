af91ba06950e35eb10421d0cc2727b5272240da8
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-07 21:35:14 +0900

    [Bronze I] Title: 색종이, Time: 76 ms, Memory: 31352 KB -BaekjoonHub

--- 백준/Bronze/2563. 색종이/색종이.py
+++ 백준/Bronze/2563. 색종이/색종이.py
@@ -1,10 +1,10 @@
 T = int(input())
-area_set = set()
+area_set = list()
 
 for n in range(T):
     x, y = map(int, input().split())
     for i in range(10):
         for j in range(10):
-            area_set.add((x + i, y + j))
+            area_set.append((x + i, y + j))
 
-print(len(area_set))
+print(len(set(area_set)))
