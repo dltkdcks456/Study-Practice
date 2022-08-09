a32d6531f68f048f4d2ab4956cc64209beac17fc
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-09 21:02:33 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -5,30 +5,28 @@ def board_check(x, y, board):                           # 체스 보드의 변�
 
     for i in range(8):                                  # 가로 위치
         for j in range(8):                              # 세로 위치
-            check = board[x + i][y + j + (8 - 2 * j - 1) * (i % 2)]
+            check = board[x + i][y + j + (7 - 2 * j) * (i % 2)]
             if j % 2 == 0:                              # WB 문양의 반복
                 if check == 'B':
                     WB_board += 1
-            else:
-                if check == 'W':
-                    WB_board += 1
-
-            if j % 2 == 0:                              # BW 문양의 반복
                 if check == 'W':
                     BW_board += 1
             else:
+                if check == 'W':
+                    WB_board += 1
                 if check == 'B':
                     BW_board += 1
 
     return min(WB_board, BW_board)
 
-x, y = map(int, input().split())
+x, y = map(int, sys.stdin.readline().split())
 board = [sys.stdin.readline().rstrip() for i in range(x)]
-chg_list = []
+chg_list = 64
 
 for m in range(x - 7):
     for n in range(y - 7):
         chg = board_check(m, n, board)
-        chg_list.append(chg)
+        if chg < chg_list:
+            chg_list = chg
 
-print(min(chg_list))
+print(chg_list)
