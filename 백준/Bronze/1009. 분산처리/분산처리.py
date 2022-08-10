ca5a9b6c04592f54d11debd6c983516a80008c40
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-11 00:08:15 +0900

    [Bronze II] Title: 분산처리, Time: 100 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,12 +1,25 @@
-dict1 = {1: [1], 2: [2, 4, 8, 6], 3: [3, 9, 7, 1], 4: [4, 6], 5: [5], 6: [6], 7: [7, 9, 3, 1], 8: [8, 4, 2, 6], 9: [9, 1], 0: [10]}
-
-T = int(input())
-for i in range(T):
-    key, repeat = map(int, input().split())
-    key = key % 10
-    k_li = dict1[key]
-    if len(k_li) == 1:
-        print(k_li[0])
-    else:
-        rest = repeat % len(k_li)
-        print(k_li[rest - 1])
+t = int(input())
+a2 = [6,2,4,8]
+a3 = [1,3,9,7]
+a4 = [6,4]
+a7 = [1,7,9,3]
+a8 = [6,8,4,2]
+a9 = [1,9]
+for i in range(t):
+    n,m = map(int,input().split())
+    if n%10 == 0 :
+        print(10)
+    elif n%10 == 2:
+        print(a2[m%4])
+    elif n%10 == 3:
+        print(a3[m%4])  
+    elif n%10 == 4:
+        print(a4[m%2])   
+    elif n%10 == 1 or n%10 == 5 or n%10 == 6:
+        print(n%10)
+    elif n%10 == 7:
+        print(a7[m%4]) 
+    elif n%10 == 8:
+        print(a8[m%4])  
+    elif n%10 == 9:
+        print(a9[m%2]) 
