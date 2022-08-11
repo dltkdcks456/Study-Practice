d9e45b944f1103244e66c69625588d213d17d8f5
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-12 00:15:05 +0900

    [Silver V] Title: 단어 정렬, Time: 1888 ms, Memory: 34860 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,10 +1,12 @@
 import sys
+from collections import deque
+
 N = int(sys.stdin.readline())
-text = [sys.stdin.readline().rstrip() for _ in range(N)]
-len_text = sorted(set([len(i) for i in text]))
-ans = []
+text = deque([sys.stdin.readline().rstrip() for _ in range(N)])
+len_text = sorted(set(deque([len(i) for i in text])))
+ans = deque([])
 for i in len_text:
-    arr = []
+    arr = deque([])
     for j in text:
         if len(j) == i and j not in arr:
             arr.append(j)
