28c50f9498c155e766182d36052554174289fadf
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-14 17:58:54 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 216 ms, Memory: 53464 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -3,5 +3,5 @@ import sys
 N, M = map(int, sys.stdin.readline().rstrip().split())
 po_li = [0] + [sys.stdin.readline().rstrip() for _ in range(N)]
 po_dict = dict(zip(po_li, range(N + 1)))
-ans = [po_li[int(t)] if (t:= sys.stdin.readline().rstrip()).isdigit() else po_dict[t]  for _ in range(M)]
-print('\n'.join(map(str,ans)))
+ans = [po_li[int(t)] if (t:= sys.stdin.readline().rstrip()).isdigit() else str(po_dict[t])  for _ in range(M)]
+print('\n'.join(ans))
