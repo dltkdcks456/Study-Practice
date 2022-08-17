ee84bcc614fb8475cbcaebe5e898d6ce573e3215
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-17 21:44:57 +0900

    [D2] Title: 파스칼의 삼각형, Time: 147 ms, Memory: 58,508 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,14 +1,9 @@
-from collections import deque
-
-T = int(input())
-for test in range(T):
-    pascal = deque([1])
+for test in range(int(input())):
+    pascal = [1]
     N = int(input())
-    n = 1
     print(f'#{test + 1}', pascal[0], sep = '\n')
-    while n < N:
-        n += 1
+    while len(pascal) < N:
         for i in range(len(pascal) - 1, 0, -1):
             pascal[i] = pascal[i] + pascal[i - 1]
-        pascal += [1]
+        pascal.append(1)
         print(' '.join(map(str, pascal)))
