97580d108bf79d8d5457dbe5b12869a1d8abe375
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-08-24 08:27:15 +0900

    [Silver III] Title: 참외밭, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,17 +1,14 @@
 N = int(input())
-li = []
-for _ in range(6):
-    li.append(list(map(int, input().split())))
+li = [list(map(int, input().split())) for _ in range(6)]
+
 D = [1, 4, 2, 3]
 d = D.index(li[0][0]) - 1
-maxc = 0
-maxr = 0
+maxc = maxr = del_a = 0
 flag = False
-delete_area = 0
-for i in range(len(li)):
+for i in range(6):
     d = (d + 1) % 4
     if li[i][0] != D[d] and flag == False:
-        delete_area = li[i][1] * li[i - 1][1]
+        del_a = li[i][1] * li[i - 1][1]
         flag = True
         
     if li[i][0] == 1 or li[i][0] == 2:
@@ -22,8 +19,8 @@ for i in range(len(li)):
             maxc = li[i][1]
 
 if flag == False:
-    delete_area = li[0][1] * li[-1][1]
+    del_a = li[0][1] * li[-1][1]
 
 total = maxr * maxc
-ans = (total - delete_area) * N
+ans = (total - del_a) * N
 print(ans)
