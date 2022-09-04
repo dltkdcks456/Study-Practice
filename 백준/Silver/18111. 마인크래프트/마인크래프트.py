c201d8d1ca1a4db688ad3dc3e27e141424a93df0
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-04 13:12:11 +0900

    [Silver II] Title: 마인크래프트, Time: 716 ms, Memory: 214732 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,39 @@
+import sys
+
+N, M, B = map(int, sys.stdin.readline().split())
+block = []
+for _ in range(N):
+    block.extend(map(int, sys.stdin.readline().split()))
+
+minV = min(block)
+maxV = max(block)
+
+ans_time = 9999999999999
+ans_height = 0
+
+for i in range(minV, maxV + 1):
+    height = block[:]
+    sum_minus = sum_plus = time = 0
+
+    for j in range(N * M):
+        height[j] -= i
+        if height[j] < 0:
+            sum_minus += height[j]
+        else:
+            sum_plus += height[j]
+
+    if sum_minus + sum_plus + B < 0:
+        continue
+    else:
+        if sum_minus <= B:
+            time += abs(sum_minus) + 2 * (sum_plus)
+        else:
+            rest = sum_minus + B
+            fill = sum_plus + rest
+            time += abs(sum_minus) + 3 * rest + 2 * fill
+
+    if time <= ans_time:
+        ans_time = time
+        ans_height = i
+
+print(ans_time, ans_height)
