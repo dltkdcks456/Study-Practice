0f6f6bc5dd71e4c9b3ace2405e0302fe9cc716a9
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-13 09:16:51 +0900

    [Silver I] Title: 정수 삼각형, Time: 176 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,7 +1,9 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 dp = []
 for _ in range(n):
-    dp.append(list(map(int, input().split())))
+    dp.append(list(map(int, sys.stdin.readline().split())))
 for i in range(1, n):
     for j in range(i + 1):
         if j == 0:
