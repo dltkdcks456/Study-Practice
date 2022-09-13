29fb838aa2c3a1499ea9e3d8404bf53048b65dce
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-13 21:18:37 +0900

    [Silver I] Title: 트리 순회, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,54 +1,31 @@
-def preorder(n):
-    if n:
-        print(chr(n + 64), end = '')
-        if ch1[n]:
-            preorder(ord(ch1[n]) - 64)
-        else:
-            preorder(ch1[n])
-        if ch2[n]:
-            preorder(ord(ch2[n]) - 64)
-        else:
-            preorder(ch2[n])
+def preorder(node):
+    if node == '.':
+        return
+    print(node, end='')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
 
-def inorder(n):
-    if n:
-        if ch1[n]:
-            inorder(ord(ch1[n]) - 64)
-        else:
-            inorder(ch1[n])
-        print(chr(n + 64), end='')
-        if ch2[n]:
-            inorder(ord(ch2[n]) - 64)
-        else:
-            inorder(ch2[n])
+def inorder(node):
+    if node == '.':
+        return
+    inorder(tree[node][0])
+    print(node, end='')
+    inorder(tree[node][1])
 
-def postorder(n):
-    if n:
-        if ch1[n]:
-            postorder(ord(ch1[n]) - 64)
-        else:
-            postorder(ch1[n])
-        if ch2[n]:
-            postorder(ord(ch2[n]) - 64)
-        else:
-            postorder(ch2[n])
-        print(chr(n + 64), end='')
-
+def postorder(node):
+    if node == '.':
+        return
+    postorder(tree[node][0])
+    postorder(tree[node][1])
+    print(node, end='')
 
 N = int(input())
-par = [0] * 27
-for j in range(65, 91):
-    par[j - 64] = chr(j)
-ch1 = [0] * 27
-ch2 = [0] * 27
-for i in range(1, N + 1):
-    p, c1, c2 = input().split()
-    if c1 != '.':
-        ch1[ord(p) - 64] = c1
-    if c2 != '.':
-        ch2[ord(p) - 64] = c2
-preorder(1)
+tree = {}
+for _ in range(N):
+    parent, left, right = input().split()
+    tree[parent] = (left, right)
+preorder('A')
 print()
-inorder(1)
+inorder('A')
 print()
-postorder(1)
+postorder('A')
