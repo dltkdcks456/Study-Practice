06dba22f85338bc79d89a2a66c9d1742425fdf5e
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-15 21:24:04 +0900

    [Silver II] Title: 최대 힙, Time: 144 ms, Memory: 36764 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,41 +1,15 @@
 import sys
+from heapq import *
+
 input=sys.stdin.readline
 
-def enq(n):
-    global last
-    last += 1
-    heap[last] = n
-    c = last
-    p = c // 2
-    while p and heap[p] < heap[c]:
-        heap[c], heap[p] = heap[p], heap[c]
-        c = p
-        p = c // 2
 
-def deq():
-    global last
-    temp = heap[1]
-    heap[1] = heap[last]
-    last -= 1
-    p = 1
-    c = p * 2
-    while c <= last:
-        if c + 1 <= last and heap[c] < heap[c + 1]:
-            c += 1
-        if heap[p] < heap[c]:
-            heap[c], heap[p] = heap[p], heap[c]
-            p = c
-            c = p * 2
-        else:
-            break
-    return temp
 
 N = int(input())
-heap = [0] * 100001
-last = 0
+heap = []
 for _ in range(N):
     x = int(input())
     if x:
-        enq(x)
+        heappush(heap, -x)
     else:
-        print(deq() if last != 0 else 0)
+        print(-heappop(heap) if heap else 0)
