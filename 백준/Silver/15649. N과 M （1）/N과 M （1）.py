5f958b491a2333f18961867dea3329c22c7b6464
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-17 07:51:05 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,18 @@
-import sys
+N, M = map(int, input().split())
+li = list(range(1, N + 1))
 
-n, m = map(int, sys.stdin.readline().split())
-n_list = list(range(1, n + 1))
-stack = [-1] * m
-
-def f(n, m):
+def perm(n, m):
     if n == m:
-        for i in stack:
-            print(i, end = ' ')
-        print()
+        print(*chosen)
         return
-    for i in range(len(n_list)):
-        if n_list[i] in stack:
-            continue
-        stack[n] = n_list[i]
-        f(n + 1, m)
-        stack[n] = -1
-f(0, m)
+    else:
+        for i in range(len(li)):
+            if visited[i] == 0:
+                chosen[n] = li[i]           # chosen의 n번째에 넣을 숫자 선택
+                visited[i] = 1              # 방문한 곳은 1로 변경
+                perm(n + 1, m)
+                visited[i] = 0              # 재귀 복귀 시 방문했던 곳 0으로 원복
+
+chosen = [-1] * M                           # 내가 골라서 담을 리스트
+visited = [0] * len(li)                     # 이미 입력한 수를 확인할 리스트
+perm(0, M)
