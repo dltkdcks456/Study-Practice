3cd7191a1c9a08192e1032425d11ae3ed921ac57
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-19 13:36:16 +0900

    [Silver III] Title: 소수 구하기, Time: 576 ms, Memory: 38512 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,10 @@
-N, M = map(int, input().split())
-
-number_list = [True] * (M + 1)
-
-for i in range(int(M ** 0.5 + 1)):
-    if i == 0 or i == 1:
-        number_list[i] = False
-    else:
-        for j in range(i + i, M + 1, i):
-            number_list[j] = False
-
-prime_number = [idx for idx in range(len(number_list)) if number_list[idx] == True]
-for prime in prime_number:
-    if N <= prime <= M:
-        print(prime)
+import sys
+M, N = map(int, sys.stdin.readline().split())
+num = [1] * (N + 1)
+num[0] = num[1] = 0
+for i in range(2, N + 1):
+    if num[i] == 1:
+        if i >= M:
+            print(i)
+        for j in range(2, (N // i) + 1):
+            num[i * j] = 0
