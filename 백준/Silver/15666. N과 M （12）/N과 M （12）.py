2d43dea1c5eaf617db993bf789682f69130941fb
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-20 21:26:49 +0900

    [Silver II] Title: N과 M (12), Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,19 +1,21 @@
 import sys
-N, M = map(int, sys.stdin.readline().split())
-li = sorted(list(map(int, sys.stdin.readline().split())))
-chosen = [-1] * M
-group = set()
 
 def perm(n, m, j):
     if n == m:
-        if tuple(chosen) not in group:
-            group.add(tuple(chosen))
-            print(*chosen)
+        if tuple(chosen) in group:
             return
         else:
+            group.add(tuple(chosen))
+            print(' '.join(map(str,chosen)))
             return
     else:
-        for i in range(j, N):
+        for i in range(j, len(li)):
             chosen[n] = li[i]
             perm(n + 1, m, i)
+
+N, M = map(int, sys.stdin.readline().split())
+li = sorted(list(map(int, sys.stdin.readline().split())))
+group = set()
+chosen = [-1] * M
 perm(0, M, 0)
+
