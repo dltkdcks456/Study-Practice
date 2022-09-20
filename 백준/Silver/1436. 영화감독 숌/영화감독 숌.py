7ce1f6145e12a9546333041e4fbbc4754a8662a8
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-09-20 23:05:02 +0900

    [Silver V] Title: 영화감독 숌, Time: 136 ms, Memory: 34520 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,10 +1,31 @@
 import sys
-N = int(sys.stdin.readline())
-cnt = 0
-i = 666
-num = '666'
-while cnt < N:
-    if num in str(i):
-        cnt += 1
-    i += 1
-print(i - 1)
+from collections import deque
+
+n = int(sys.stdin.readline())
+a = '666'
+li = [str(i) for i in range(10)]
+result = deque()
+def perm(n, m):
+    if n == m:
+        result.append(int(''.join(chosen)))
+        return
+    else:
+        flag = True
+        for j in range(len(li)):
+            if chosen[n] == a:
+                flag = False
+                break
+            else:
+                chosen[n] = li[j]
+            perm(n + 1, m)
+        if not flag:
+            perm(n + 1, m)
+
+for k in range(5):
+    chosen = [-1] * 5
+    chosen[k] = a
+    perm(0, 5)
+
+print(sorted(set(result))[n - 1])
+
+
