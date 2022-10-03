317227a397d47e0b83beff0e2cdbbf350cfd2ad8
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-10-03 20:38:26 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1540 ms, Memory: 130884 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -7,9 +7,6 @@ def find_set(x):                            # 대표값을 찾는 함수
         x = rep[x]
     return x
 
-def union_set(a, b):                        # 집합을 합치는 함수
-    rep[find_set(b)] = find_set(a)          #  b의 대표값을 a의 대표값으로 변경
-
 V, E = map(int, input().split())
 data = []                                   # 입력값 저장 리스트
 rep = [i for i in range(V + 1)]             # 대표원소 저장 리스트
@@ -20,7 +17,9 @@ for _ in range(E):
 s = 0                                       # 합을 구할 변수
 for j in range(E):                          # 순환 트리가 아니면서 최솟값을 계속 더해가기
     w, u, v = heapq.heappop(data)
-    if find_set(u) != find_set(v):
+    Ru = find_set(u)
+    Rv = find_set(v)
+    if Ru != Rv:
         s += w
-        union_set(u, v)
+        rep[Rv] = Ru
 print(s)
