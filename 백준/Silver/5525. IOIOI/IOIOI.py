1edf2fd46b76aa65dd5c7dfe55bf2e4892f975dd
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-10-07 23:06:59 +0900

    [Silver I] Title: IOIOI, Time: 276 ms, Memory: 32796 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,29 @@
+import sys
+
+N = int(input())
+M = int(input())
+S = input()
+
+P = 'IOI'
+idx = 0
+cnt = 0
+num = []
+while idx <= M - 2:
+    flag = True
+    if S[idx: idx + 3] == P:
+        cnt += 1
+        idx += 2
+        flag = False
+    else:
+        num.append(cnt)
+        cnt = 0
+        idx += 1
+else:
+    if not flag:
+        num.append(cnt)
+ans = 0
+for i in num:
+    if i >= N:
+        ans += i - N + 1
+
+print(ans)
