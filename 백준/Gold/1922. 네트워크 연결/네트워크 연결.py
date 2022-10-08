4d065186e7e0c89d4453ab91747f3b719d7fb0ae
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-10-08 23:16:07 +0900

    [Gold IV] Title: 네트워크 연결, Time: 400 ms, Memory: 39804 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,31 +1,31 @@
 import sys
-from collections import deque
 input=sys.stdin.readline
 
-def find_set(x):
-    while rep[x] != x:
-        x = rep[x]
-    return x
-
-def union_set(m, n):
-    rep[find_set(n)] = find_set(m)
-
-def kruskal(li):
-    cost = 0
-    for vertex in li:
-        w, a, b = vertex
-        if find_set(a) != find_set(b):
-            cost += w
-            union_set(a, b)
-    return cost
+def prim1(r, V):
+    MST = [0] * (V + 1)
+    key = [float('inf')] * (V + 1)
+    key[r] = 0
+    for _ in range(V - 1):
+        u = 0
+        minV = float('inf')
+        for i in range(1, V + 1):
+            if MST[i] == 0 and key[i] < minV:
+                u = i
+                minV = key[i]
+        MST[u] = 1
+        for j in range(1, V + 1):
+            if MST[j] == 0 and 0 < arr[u][j]:
+                key[j] = min(arr[u][j], key[j])
+    return sum(key[1:])
 
 N = int(input())
 M = int(input())
-data = []
-rep = [i for i in range(N + 1)]
+arr = [[0] * (N + 1) for _ in range(N + 1)]
 for _ in range(M):
     a, b, w = map(int, input().split())
-    data.append([w, a, b])
-data.sort()
-print(kruskal(data))
+    if a != b:
+        arr[a][b] = w
+        arr[b][a] = w
+
+print(prim1(1, N))
 
