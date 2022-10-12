0ab61b0e53e720bfdf25a5a9b94000bd67a0e1b3
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-10-12 21:26:09 +0900

    [Gold IV] Title: 트리, Time: 292 ms, Memory: 35680 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -2,23 +2,23 @@ import sys
 
 def dfs(n, par):
     global flag
-    for w in adjList[n]:                                # 인접 노드로 진행
-        if w != par:                                    # 부모 노드와 다른 곳에서만 탐색
-            if visited[w] == 0:                         # 방문하지 않은 경우
-                visited[w] = 1                          # 방문 처리
+    for w in adjList[n]:
+        if w != par:
+            if visited[w] == 0:
+                visited[w] = 1
                 dfs(w, n)
             else:
                 flag = False
 
 
-test = 1                                                # 테스트케이스 출력
+test = 1
 while True:
     n, m = map(int, sys.stdin.readline().split())
-    if n == 0 and m == 0:                               # 0, 0 입력 시 정지
+    if n == 0 and m == 0:
         break
 
     adjList = [[] for _ in range(n + 1)]
-    visited = [0] * (n + 1)                             # 방문 기록을 할 저장공간
+    visited = [0] * (n + 1)
     for _ in range(m):
         a, b = map(int, sys.stdin.readline().split())
         adjList[a].append(b)
@@ -26,14 +26,14 @@ while True:
 
     result = 0
     for i in range(1, n + 1):
-        if visited[i] == 0:                             # 방문하지 않으면 dfs진행
-            flag = True                                 # 순환이 있는지 확인할 신호
-            visited[i] = 1                              # 탐색 시작점 방문 처리
+        if visited[i] == 0:
+            flag = True
+            visited[i] = 1
             dfs(i, -1)
-            if flag:                                    # 순환이 없으면 트리 개수 1 증가
+            if flag:
                 result += 1
 
-    if result == 0:                                     # 출력 부분
+    if result == 0:
         print(f'Case {test}: No trees.')
     elif result == 1:
         print(f'Case {test}: There is one tree.')
