9b12729d3c59b48e2d79eff171551dad9adc5a92
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-10-12 21:19:57 +0900

    [Gold IV] Title: 트리, Time: 304 ms, Memory: 36700 KB -BaekjoonHub

---
+++ 백준/Gold/4803. 트리/트리.py
@@ -0,0 +1,48 @@
+import sys
+
+def dfs(n, par):
+    global flag
+    for w in adjList[n]:
+        if w != par:
+            if visited[w] == 0:
+                visited[w] = 1
+                check.add(w)
+                dfs(w, n)
+            else:
+                if w in check:
+                    flag = False
+
+
+test = 1
+while True:
+    n, m = map(int, sys.stdin.readline().split())
+
+    if n == 0 and m == 0:
+        break
+
+    adjList = [[] for _ in range(n + 1)]
+    visited = [0] * (n + 1)
+    for _ in range(m):
+        a, b = map(int, sys.stdin.readline().split())
+        adjList[a].append(b)
+        adjList[b].append(a)
+
+
+    result = 0
+    check = set()
+    for i in range(1, n + 1):
+        if visited[i] == 0:
+            flag = True
+            visited[i] = 1
+            check.add(i)
+            dfs(i, -1)
+            if flag:
+                result += 1
+
+    if result == 0:
+        print(f'Case {test}: No trees.')
+    elif result == 1:
+        print(f'Case {test}: There is one tree.')
+    else:
+        print(f'Case {test}: A forest of {result} trees.')
+    test += 1
