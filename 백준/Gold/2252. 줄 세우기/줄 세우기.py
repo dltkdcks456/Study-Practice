c82666c0ad720b288ffcc40b00e5b9eadb0c32bb
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-10-20 21:42:07 +0900

    [Gold III] Title: 줄 세우기, Time: 1728 ms, Memory: 120836 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -3,7 +3,6 @@ from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
 connect = [0] * (N + 1)
-visited = [0] * (N + 1)
 adjList = [[] for _ in range(N + 1)]
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
@@ -14,7 +13,7 @@ q = deque()
 for i in range(1, N + 1):
     if connect[i] == 0:
         q.append(i)
-        visited[i] = 1
+        connect[i] = -1
 
 while q:
     v = q.popleft()
@@ -23,6 +22,6 @@ while q:
         connect[w] -= 1
 
     for j in range(1, N + 1):
-        if visited[j] == 0 and connect[j] == 0:
+        if connect[j] == 0:
             q.append(j)
-            visited[j] = 1
+            connect[j] = -1
