5978900641d7e3540924fa48f6806e53ca8e93d6
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-10-30 12:18:08 +0900

    [Gold V] Title: LCS, Time: 692 ms, Memory: 55032 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,15 +1,19 @@
 import sys
+input = sys.stdin.readline
 
-text1 = sys.stdin.readline().rstrip()
-text2 = sys.stdin.readline().rstrip()
 
-N = len(text1)
-M = len(text2)
-Z = [[0] * (N + 1) for _ in range(M + 1)]
-for r in range(1, M + 1):
-    for c in range(1, N + 1):
+text1 = list(input().rstrip())
+text2 = list(input().rstrip())
+
+N1 = len(text1)
+N2 = len(text2)
+
+dp = [[0] * (N1 + 1) for _ in range(N2 + 1)]
+for r in range(1, N2 + 1):
+    for c in range(1, N1 + 1):
         if text1[c - 1] == text2[r - 1]:
-            Z[r][c] = Z[r - 1][c - 1] + 1
+            dp[r][c] = dp[r - 1][c - 1] + 1
         else:
-            Z[r][c] = max(Z[r - 1][c], Z[r][c - 1])
-print(Z[M][N])
+            dp[r][c] = max(dp[r - 1][c], dp[r][c - 1])
+
+print(dp[N2][N1])
