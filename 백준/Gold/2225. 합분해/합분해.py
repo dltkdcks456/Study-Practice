264d06c4a777eafa4c4580739e74175a15b511eb
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2022-12-20 17:11:01 +0900

    [Gold V] Title: 합분해, Time: 48 ms, Memory: 32728 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -1,23 +1,24 @@
 import sys
-input = sys.stdin.readline
-
+# input = sys.stdin.readline
 '''
-조합을 활용해서 N이 주어질 경우 K개 이하로 자르는 방식으로 구할 예정
-즉 K분할 되는 경우, K-1 분할 되는 경우의 수 ...과 같이 나누어 합하면 답을 구할 수 있다.
+개수가 0일 때 0을 만드는 경우의 수는 1개이다(dp[0][0])
+dp[i][j]는 N이 i보다 작거나 같은 것들을 만들 수 있는 경우의 수들의 합이 와야한다.
+
+N: 5, K: 2 일 경우
+N\K   0  1  2
+0    [1, 1, 1]
+1    [0, 1, 2]
+2    [0, 1, 3]
+3    [0, 1, 4]
+4    [0, 1, 5]
+5    [0, 1, 6]
 '''
+N, K = map(int, input().split())
+dp = [[1] * (K + 1)] + [[0] * (K + 1) for _ in range(N)]
+for c in range(1, K + 1):
+    for r in range(1, N + 1):
+        dp[r][c] = dp[r][c - 1] + dp[r - 1][c]
 
-#조합에 대한 함수(nCm)
-def comb(n, m):
-    temp = 1
-    for i in range(m):
-        temp *= n - i
-    for j in range(1, m + 1):
-        temp //= j
-    return temp
-
-
-N, K = map(int, input().split()) # 합의 크기 N, 개수 K
-ans = 0
-for k in range(K - 1, -1, -1):
-    ans += comb(N - 1, k) * comb(K, K - k - 1)
-print(ans % 1000000000)
+# for i in dp:
+#     print(i)
+print(dp[N][K] % 1000000000)
