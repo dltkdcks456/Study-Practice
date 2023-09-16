6b653f2f474f8b5f91142964c744cc2cab8b2273
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2023-09-16 18:49:33 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1024 ms, Memory: 110540 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,42 +2,39 @@ import java.io.*;
 
 public class Main {
     static int[] arr;
+    static int[] tmp;
+    static void merge(int s, int e) {
+        if (s < e) {
+            int m = (s + e) / 2;
+            merge(s, m);
+            merge(m + 1, e);
 
-    static int[] merge(int s, int e) {
-        if(s == e) {
-            return new int[]{arr[s]};
-        }
-        int m = (s + e) / 2;
-        int[] A = merge(s, m);
-        int[] B = merge(m + 1, e);
+            int l = s, r = m + 1;
+            int idx = l;
 
-        int lenA = A.length;
-        int lenB = B.length;
-
-        int[] C = new int[lenA + lenB];
-        int l = 0, r = 0;
-        int idx = 0;
-
-        while(l < lenA || r < lenB) {
-            if (r == lenB || (l < lenA && A[l] < B[r])) {
-                C[idx++] = A[l++];
-            } else {
-                C[idx++] = B[r++];
+            while(l <= m || r <= e) {
+                if (r > e || (l <= m && arr[l] < arr[r])) {
+                    tmp[idx++] = arr[l++];
+                } else {
+                    tmp[idx++] = arr[r++];
+                }
+            }
+            for (int i = s; i <= e; i++) {
+                arr[i] = tmp[i];
             }
         }
-        return C;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         arr = new int[N];
-
+        tmp = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        int answer[] = merge(0, N - 1);
-        for(int i : answer) {
+        merge(0, N - 1);
+        for(int i : arr) {
             bw.write(i + "\n");
         }
         bw.flush();
