be8f1e1391855474c9add5c9f754c13eba658601
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2023-09-18 22:40:25 +0900

    [Gold V] Title: 트리, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static int[] parents;
+    static int cnt;
+    static void DFS(int node) {
+        boolean flag = true;
+        for (int i = 0; i < graph[node].size(); i++) {
+            flag = false;
+            DFS(graph[node].get(i));
+        }
+        if (flag) cnt++;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int root = 0;
+        cnt = 0;
+        StringTokenizer st;
+        parents = new int[N];
+        graph = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            parents[i] = num;
+            if (num != -1) {
+                graph[num].add(i);
+            } else {
+                root = i;
+            }
+        }
+        int EraseNode = Integer.parseInt(br.readLine());
+
+        if (EraseNode != root) {
+            graph[parents[EraseNode]].remove(Integer.valueOf(EraseNode));
+            DFS(root);
+        }
+
+
+        System.out.println(cnt);
+    }
+}
