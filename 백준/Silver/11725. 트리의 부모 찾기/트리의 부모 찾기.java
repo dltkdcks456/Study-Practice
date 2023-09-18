69ee3b3f3afef1b2036969d3db40e458151a324b
Lee Sangchan <109336772+dltkdcks456@users.noreply.github.com>
2023-09-18 22:08:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1332 ms, Memory: 77860 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static ArrayList<Integer> graph[];
+    static int[] parents;
+    static void find(int currNode, int prevNode) {
+        parents[currNode] = prevNode;
+        for (int i = 0; i < graph[currNode].size(); i++) {
+            if (graph[currNode].get(i) != prevNode) {
+                find(graph[currNode].get(i), currNode);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        parents = new int[N + 1];
+        find(1, 0);
+        for (int i = 2; i < N + 1; i++) {
+            System.out.println(parents[i]);
+        }
+    }
+}
